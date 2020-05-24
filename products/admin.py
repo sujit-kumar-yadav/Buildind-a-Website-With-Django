2df24a8efe3ac7e4d18346cd83888cdd5fe580a5
Sujit Kumar Yadav <sujit4599.official@gmail.com>
2020-05-25 02:35:44 +0530

    Add files via upload

---
+++ products/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from .models import Product
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'stock')
+
+
+# Register your models here.
+admin.site.register(Product, ProductAdmin)
+
